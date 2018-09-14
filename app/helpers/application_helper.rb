24e5316f1702b59e5f1881fc8df929e317b72245
Ubuntu <ubuntu@ip-172-31-29-17.ec2.internal>
2018-09-14 01:59:32 +0000

    added footer with app name and version nr

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def self.GetAppName()
+    return "Simple Webmail System by onTy"
+  end
+
+  def self.GetAppVersion()
+    return "0.1"
+  end
+
+  def self.GetAppUrl()
+    return "https://github.com/artOfSoftware/onty-webmail-ruby.git"
+  end
+
 end
